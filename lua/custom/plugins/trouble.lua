1a8c3b4d9c9ddd95b404d1feb18ef544a87f1f8c
Kasey Morland <morlandkc@gmail.com>
2024-01-18 13:19:32 -0600

    Added latest kickstart nvim

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,3 +1,12 @@
 return {
-	"folke/trouble.nvim"
+ "folke/trouble.nvim",
+
+ dependencies = { "nvim-tree/nvim-web-devicons" },
+ opts = {
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+
+  -- refer to the configuration section below
+
+ },
 }
